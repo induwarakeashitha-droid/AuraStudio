92b1803335722ba0c43e1d89324dc6d06115b492
unknown <induwarakeashitha@icloud.com>
2026-08-04 02:14:05 +0530

    dsa

--- testdb.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const db = require('./src/db');
-db.query("INSERT INTO admins (email, password_hash) VALUES ('realadmin@aura.studio', 'fakehash')")
-  .then(() => console.log("INSERT WORKED"))
-  .catch(e => console.error("INSERT ERROR:", e.message))
-  .finally(() => process.exit(0));
